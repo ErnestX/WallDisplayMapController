fd02c7e7268b80bad0ef1a4c0311619a123d4470
ErnestX <ernestxiangjlnq2011@gmail.com>
2015-05-21 11:46:30 -0700

    set up UILabels in storyboard

---
+++ WallDisplayMapController/MapControl.h
@@ -0,0 +1,13 @@
+//
+//  MapControl.h
+//  WallDisplayMapController
+//
+//  Created by Jialiang Xiang on 2015-05-21.
+//  Copyright (c) 2015 Jialiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapControl : UIView
+
+@end
