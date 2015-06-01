eab43726c0883fbaa935f5b7790c1ab983b20714
ErnestX <ernestxiangjlnq2011@gmail.com>
2015-06-01 15:45:25 -0700

    added four video players

--- WallDisplayMapController/TutorialView.m
+++ WallDisplayMapController/TutorialView.m
@@ -18,13 +18,10 @@
     self = [super initWithCoder:aDecoder];
     if (self) {
         // init player layers
-        NSURL *dragURL = [[NSBundle mainBundle] URLForResource:@"IMG_0011" withExtension:@"m4v"];
-        AVPlayer* dragPlayer = [AVPlayer playerWithURL:dragURL];
-        AVPlayerLayer* dragTutorialLayer = [AVPlayerLayer playerLayerWithPlayer:dragPlayer];
-//        dragTutorialLayer.position = self.center;
-        dragTutorialLayer.frame = self.frame;
-        [self.layer addSublayer:dragTutorialLayer];
-        [dragPlayer play];
+        [self initTutorialPlayerForResource:@"IMG_0011" withExtension:@"m4v" withFrame:CGRectMake(0, 0, CGRectGetMidX(self.frame), CGRectGetMidY(self.frame))];
+        [self initTutorialPlayerForResource:@"IMG_0012" withExtension:@"m4v" withFrame:CGRectMake(CGRectGetMidX(self.frame), 0, CGRectGetMidX(self.frame), CGRectGetMidY(self.frame))];
+        [self initTutorialPlayerForResource:@"IMG_0012" withExtension:@"m4v" withFrame:CGRectMake(0, CGRectGetMidY(self.frame), CGRectGetMidX(self.frame), CGRectGetMidY(self.frame))];
+        [self initTutorialPlayerForResource:@"IMG_0011" withExtension:@"m4v" withFrame:CGRectMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame), CGRectGetMidX(self.frame), CGRectGetMidY(self.frame))];
     }
     return self;
 }
@@ -32,6 +29,27 @@
 - (void) setController: (TutorialViewController*) tvc
 {
     controller = tvc;
+}
+
+- (void)initTutorialPlayerForResource: (NSString*)r withExtension:(NSString*)e withFrame:(CGRect)f
+{
+    NSURL *dragURL = [[NSBundle mainBundle] URLForResource:r withExtension:e];
+    AVPlayer* dragPlayer = [AVPlayer playerWithURL:dragURL];
+    AVPlayerLayer* dragTutorialLayer = [AVPlayerLayer playerLayerWithPlayer:dragPlayer];
+    dragTutorialLayer.frame = f;
+    [self.layer addSublayer:dragTutorialLayer];
+    dragPlayer.actionAtItemEnd = AVPlayerActionAtItemEndNone;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playerItemDidReachEnd:) name:AVPlayerItemDidPlayToEndTimeNotification object:[dragPlayer currentItem]];
+    
+    dragPlayer.muted = YES;
+    [dragPlayer play];
+}
+
+- (void)playerItemDidReachEnd:(NSNotification*)n
+{
+    AVPlayerItem *p = [n object];
+    [p seekToTime:kCMTimeZero];
 }
 
 - (void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
