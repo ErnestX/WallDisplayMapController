652f053d2a5f6324a2a3d36d4366a5f2ed040481
ErnestX <ernestxiangjlnq2011@gmail.com>
2016-02-17 02:31:01 -0800

    did some testing on resizing collection view. Looks like I need to subclass the layout object to avoid unintended scorlling when resizing.

--- WallDisplayMapController/HistoryContainerView.m
+++ WallDisplayMapController/HistoryContainerView.m
@@ -20,7 +20,28 @@ UICollectionView* historyBarView;
     
     self.backgroundColor = [UIColor darkGrayColor];
     
+    UIButton* testButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [testButton setTitle:@"test" forState:UIControlStateNormal];
+    testButton.frame = CGRectMake(500, 500, 50, 50);
+    [testButton addTarget:self action:@selector(testButtonPressed:) forControlEvents:UIControlEventTouchDown];
+    [testButton addTarget:self action:@selector(testButtonReleased:) forControlEvents:UIControlEventTouchUpInside];
+    [testButton addTarget:self action:@selector(testButtonReleased:) forControlEvents:UIControlEventTouchUpOutside];
+    [self addSubview:testButton];
+    
     return self;
+}
+
+- (void)testButtonPressed:(id)sender {
+    NSLog(@"test button pressed");
+//    [UIView animateWithDuration:0.5 animations:^(void){
+        historyBarView.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, self.frame.size.width, 500);
+//    }];
+}
+
+- (void)testButtonReleased:(id)sender {
+//    [UIView animateWithDuration:0.5 animations:^(void){
+        historyBarView.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, self.frame.size.width, 150);
+//    }];
 }
 
 - (void) setUpHistoryBar: (UICollectionView *) historyBar {
